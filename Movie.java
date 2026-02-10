31f9046ede37e91745b592b4eb127b7546ec9495
RohitDen <157422362+RohitDen@users.noreply.github.com>
2026-02-10 13:40:56 -0800

    Create Movie class with attributes and methods
    Implemented Movie class with attributes, constructor, accessors, and mutators.

---
+++ Movie.java
@@ -0,0 +1,57 @@
+public class Movie {
+  private String title;
+  private int duration;
+  private int rating;
+  
+  /*** Constructor ****/
+  public Movie(String t, int d)
+  {
+    title = t;
+    duration = d;
+    rating = 0;
+  }
+  
+   /*** Accessor methods ***/
+  public String getTitle() {
+    return title;
+  }
+
+  public int getDuration() {
+    return duration;
+  }
+  
+  public int getRating() {
+    return rating;
+  }
+  
+  public String toString() 
+  {
+    String info = "\"" + title + "\", duration is " + duration;
+    if (rating != 0) 
+    { 
+      info += ", rating is " + rating;
+    }
+    return info;
+  }
+
+  /*** Mutator methods ***/
+  public void adjustRating(int r) {
+    if (r>=1 && r<=10){
+          rating = r;
+    }
+  }
+  public void setTitle(String t) {
+    title = t;
+  }
+
+  public void setDuration(int d) {
+    duration = d;
+  }
+  public boolean equals(Movie m)
+  {
+    if (this.title.equals(m.title) && this.duration ==(m.duration)) {
+      return true;
+    }
+    return false;
+  }
+}
