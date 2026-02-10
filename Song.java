a68bbac5df999b3a27908b3f682b97be9d6048c3
RohitDen <157422362+RohitDen@users.noreply.github.com>
2026-02-10 13:41:33 -0800

    Create Song class with attributes and methods
    Implemented a Song class with title and rating attributes, including constructor, accessor, mutator methods, and an equals method.

---
+++ Song.java
@@ -0,0 +1,50 @@
+public class Song {
+  private String title;
+  private int rating;
+  
+  /*** Constructor ****/
+  public Song(String t)
+  {
+    title = t;
+    rating = 0;
+  }
+  
+   /*** Accessor methods ***/
+  public String getTitle() {
+    return title;
+  }
+  
+  public int getRating() {
+    return rating;
+  }
+  
+  public String toString() 
+  {
+    String info = "\"" + title + "\", Rating: " + rating;
+    if (rating != 0) 
+    { 
+      info += ", rating is " + rating;
+    }
+    return info;
+  }
+
+  /*** Mutator methods ***/
+  public void adjustRating(int r){
+    if (r >= 1 && r <= 10){
+      rating = r;
+    }
+  }
+
+  public void setTitle(String t) {
+    title = t;
+  }
+
+  public boolean equals(Song s){
+    if (this.title.equals(s.title) && this.rating == s.rating) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+}
+
